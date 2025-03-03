6b3bade2b688ba04ae4a5bed2c46233bf4032810
arrietayesleidis <arrietayesleidis@gmail.com>
2025-03-03 16:57:26 -0500

    Add files via upload

---
+++ Parcial/src/Clases/Nota.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Carlitos
+ */
+public class Nota {
+   private float nota1, nota2, nota3;
+
+    public Nota( float nota1, float nota2, float nota3) {
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;        
+    }
+   public static float CalcularNota(float nota1,float nota2,float nota3){
+         return (float) (nota1*0.3+nota2*0.3+nota3*0.4);
+    }
+
+    /**
+     * @return the nota1
+     */
+    public float getNota1() {
+        return nota1;
+    }
+
+    /**
+     * @param nota1 the nota1 to set
+     */
+    public void setNota1(float nota1) {
+        this.nota1 = nota1;
+    }
+
+    /**
+     * @return the nota2
+     */
+    public float getNota2() {
+        return nota2;
+    }
+
+    /**
+     * @param nota2 the nota2 to set
+     */
+    public void setNota2(float nota2) {
+        this.nota2 = nota2;
+    }
+
+    /**
+     * @return the nota3
+     */
+    public float getNota3() {
+        return nota3;
+    }
+
+    /**
+     * @param nota3 the nota3 to set
+     */
+    public void setNota3(float nota3) {
+        this.nota3 = nota3;
+    }
+    
+}
